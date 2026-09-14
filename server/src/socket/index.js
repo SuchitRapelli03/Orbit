eddabb2db64800df7727ac2e28b05b036efbfd7b
hanji-abhay <abhaymathur629@gmail.com>
2026-09-14 20:48:25 +0530

    feat: secure realtime board socket access

--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -1,7 +1,8 @@
 import { Server } from "socket.io";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
-
+import Board from "../models/Board.js";
+import Workspace from "../models/Workspace.js";
 
 export function createSocketServer(
   httpServer
@@ -113,39 +114,61 @@ export function createSocketServer(
       /*
         Join board room
       */
-      socket.on(
-        "board:join",
-        (boardId) => {
-
-          if (
-            typeof boardId !==
-            "string"
-          ) {
-            return;
-          }
-
-
-          const room =
-            `board:${boardId}`;
-
-
-          socket.join(room);
-
-
-          socket.to(room).emit(
-            "presence:joined",
-            {
-              userId:
-                socket.user._id,
-
-              name:
-                socket.user.name
-            }
-          );
-
+     socket.on("board:join", async (boardId) => {
+      try {
+        if (typeof boardId !== "string") {
+          return socket.emit("board:error", {
+            message: "Invalid board ID"
+          });
         }
-      );
 
+        const board = await Board.findById(boardId).select(
+          "_id workspace members"
+        );
+
+        if (!board) {
+          return socket.emit("board:error", {
+            message: "Board not found"
+          });
+        }
+
+        const workspace = await Workspace.findOne({
+          _id: board.workspace,
+          members: socket.user._id
+        }).select("_id");
+
+        if (!workspace) {
+          return socket.emit("board:error", {
+            message: "Workspace access denied"
+          });
+        }
+
+        const isBoardMember = board.members.some((memberId) =>
+          memberId.equals(socket.user._id)
+        );
+
+        if (!isBoardMember) {
+          return socket.emit("board:error", {
+            message: "Board access denied"
+          });
+        }
+
+        const room = `board:${boardId}`;
+
+        socket.join(room);
+
+        socket.to(room).emit("presence:joined", {
+          userId: socket.user._id.toString(),
+          name: socket.user.name
+        });
+      } catch (error) {
+        console.error("Board join failed:", error.message);
+
+        socket.emit("board:error", {
+          message: "Unable to join board"
+        });
+      }
+    });
 
       /*
         Leave board room
@@ -184,47 +207,40 @@ export function createSocketServer(
       /*
         Typing indicator
       */
-      socket.on(
-        "typing:start",
-        ({
-          boardId
-        }) => {
+      
 
-          socket
-            .to(`board:${boardId}`)
-            .emit(
-              "typing:start",
-              {
-                userId:
-                  socket.user._id,
-
-                name:
-                  socket.user.name
-              }
-            );
-
+     socket.on("typing:start", ({ boardId }) => {
+        if (typeof boardId !== "string") {
+          return;
         }
-      );
 
+        const room = `board:${boardId}`;
 
-      socket.on(
-        "typing:stop",
-        ({
-          boardId
-        }) => {
-
-          socket
-            .to(`board:${boardId}`)
-            .emit(
-              "typing:stop",
-              {
-                userId:
-                  socket.user._id
-              }
-            );
-
+        if (!socket.rooms.has(room)) {
+          return;
         }
-      );
+
+        socket.to(room).emit("typing:start", {
+          userId: socket.user._id.toString(),
+          name: socket.user.name
+        });
+      });
+
+      socket.on("typing:stop", ({ boardId }) => {
+        if (typeof boardId !== "string") {
+          return;
+        }
+
+        const room = `board:${boardId}`;
+
+        if (!socket.rooms.has(room)) {
+          return;
+        }
+
+        socket.to(room).emit("typing:stop", {
+          userId: socket.user._id.toString()
+        });
+      });
 
 
       socket.on(
